ed75db7f421592cd0afe32fa96658725c5c295a9
akapoor810 <anika.kapoor810@gmail.com>
2023-04-24 17:30:56 -0400

    fixing header

--- actors.php
+++ actors.php
@@ -3,6 +3,85 @@
 <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <link rel="stylesheet" href="final.css">
+        <style type="text/css">
+                .gallery {
+                                display: grid;
+                                width: auto;
+                        }
+
+                .gallery__item {
+                        cursor: pointer;
+                        overflow: hidden;
+                        border-radius: 30px;
+                }
+
+                .gallery__item img {
+                        object-fit: cover;
+                        transition: 0.3s ease-in-out;
+                }
+
+                .gallery__item img:hover {
+                        transform: scale(1.1);
+                        }
+
+                @media (max-width: 950px) {
+                        .gallery {
+                                grid-template-columns: repeat(2, 1fr);
+                        }
+                }
+
+                @media (max-width: 550px) {
+                        .gallery {
+                                grid-template-columns: repeat(1, 1fr);
+                        }
+                }
+                
+                @keyframes zoom {
+                        from {
+                                transform: scale(0);
+                        }
+                        to {
+                                transform: scale(1);
+                        }
+                }
+
+                .modal {
+                        width: 100%;
+                        height: 100%;
+                        position: fixed;
+                        top: 0;
+                        display: flex;
+                        justify-content: center;
+                        align-items: center;
+                        background-color: rgba(0, 0, 0, 0.733);
+                        animation: zoom 0.3s ease-in-out;
+                }
+                
+                .modal img {
+                        height: 75%;
+                        object-fit: cover;
+                }
+
+                button {
+                        background-color: none;
+                        border: none;
+                        color: rgba(255, 255, 255, 0.87);
+                }
+                
+                .closeBtn {
+                        background-color: transparent;
+                        font-size: 40px;
+                        position: fixed;
+                        top: 0;
+                        right: 0;
+                        cursor: pointer;
+                        transition: 0.2s ease-in-out;
+                }
+
+                .closeBtn:hover {
+                        color: rgb(255, 255, 255);
+                }
+        </style>
         <script
                 src="https://code.jquery.com/jquery-3.6.0.min.js"
                 integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
@@ -42,7 +121,7 @@
                         die("Connection failed: " . $conn->connect_error);
                 } 
 
-                $sql = "SELECT name, image, film, imdb FROM actors";
+                $sql = "SELECT fname, lname, image, film, imdb FROM actors";
                 $result = $conn->query($sql);
 
                 // Building dropdown menu to select actors/actresses
@@ -53,7 +132,7 @@
                 if ($result->num_rows > 0) {                                
                         $i = 0;
                         while ($row = $result->fetch_assoc()) {
-                                $names[$i] = $row["name"];
+                                $names[$i] = $row["fname"]." ".$row["lname"];
                                 $images[$i] = $row["image"];
                                 $films[$i] = $row["film"];
                                 $imdbs[$i] = $row["imdb"];
@@ -77,7 +156,7 @@
                         var actorIMDBs = <?php echo json_encode($imdbs); ?>;
 
                         //var dropdown = "<select style='margin: auto'>";
-                        var gallery = "<table style='text-align: center; margin: auto; width: auto'>";
+                        var gallery = "<div class='gallery' style='margin: auto'><div class='flexbox'><table style='text-align: center; margin: auto; width: auto'>";
 
                         for (let i = 0; i < actorNames.length; i++) {
                                 console.log(actorNames[i]);
@@ -89,7 +168,7 @@
                                 }
                                 
                                 console.log(actorFilms.length);
-                                gallery += "<td id='" + name + "'>" + "<img src='" + actorImages[i] + "' width='180px' height='220px';" 
+                                gallery += "<td class='gallery__item'>" + "<img src='" + actorImages[i] + "' width='180px' height='220px';" 
                                 + "<br/><br/>" + actorNames[i] + "<br/>"+ actorFilms[i] + "<br/><a href='" 
                                 + actorIMDBs[i] + "' target='_blank'>IMDB profile</a>" + "</td>";
 
@@ -99,7 +178,7 @@
                         }
 
                         //dropdown += "</select>";
-                        gallery += "</table>";
+                        gallery += "</div></table>";
 
                         var footer = "<footer><br />&copy;Screen Shuffle<footer class='navigation-align-right'>"
                                         + "<a href='https://www.instagram.com/tuftsanimalaid/' target='_blank'>"
